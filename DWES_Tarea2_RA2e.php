bf5c0190363deb78d74d664719873b3e539d87e2
Felipenamu <141154776+Felipenamu@users.noreply.github.com>
2024-10-19 17:53:33 +0200

    Add files via upload

---
+++ DWES_Tarea2_RA2e.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        //Declaración de variables
+        $nombre = "Felipe";
+        $apellido = "Navarro";
+        $dni = "26813690Z";
+        echo "<p> Nombre:  $nombre  </br> </p>";
+        echo "<p> Apellido: $apellido </br> </p>";
+        echo "<p> DNI: $dni  </br> </p>";
+        ?>
+    </body>
+</html>
