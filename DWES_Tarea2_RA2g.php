e61eb7b5a5f762b02a71607eb0c431375c2013bb
Felipenamu <141154776+Felipenamu@users.noreply.github.com>
2024-10-19 17:54:35 +0200

    Add files via upload

---
+++ DWES_Tarea2_RA2g.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $entero = 10;
+        $decimal = 8.22;
+        $booleano = true;
+        $cadena = "cadena";
+        const PI=3.14;
+        echo "La variable entera contiene : $entero<br>";
+        echo "La variable decimal contiene : $decimal<br>";
+        echo "La variable boolena contiene : $booleano<br>";
+        echo "La variable cadena contiene : $cadena<br>"; 
+        echo "La constante PI contiene : ".PI."<br>";
+        echo "La suma de la variable entera y la variable decimal es : ";
+        echo $entero+$decimal;
+        ?>
+    </body>
+</html>
